5fb929124d7021be4353bec449479836b8d64a00
ZiYan416 <Zi_Yan416@163.com>
2023-11-04 21:34:04 +0800

    The first submission carried out the basic architecture and interface design of the website

---
+++ script.js
@@ -0,0 +1,17 @@
+let navbarItem = document.querySelectorAll('.item')
+for (let i = 0; i < navbarItem.length; i++) {
+    navbarItem[i].addEventListener('click', function () {
+        for (let i = 0; i < navbarItem.length; i++) {
+            navbarItem[i].classList.remove('active')
+        }
+        this.classList.add('active')
+    })
+}
+
+var typed = new Typed(".multiple-text", {
+    strings: ['College Student', "Music Lover", "Software Engineering Apprenticeship"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
